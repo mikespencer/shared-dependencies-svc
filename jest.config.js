1d0bae0e16ded95d90167411e46290adea2ede28
Mike Spencer <mikespencema@gmail.com>
2018-04-12 19:15:40 -0400

    testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  testPathIgnorePatterns: ['/node_modules', '/jest/'],
   collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  globalSetup: './jest/setup.js',
+  globalTeardown: './jest/teardown.js',
+  testEnvironment: './jest/mongo-environment.js'
 };
